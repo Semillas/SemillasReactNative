32248279f850140bb98d945bab3d88dd0e2c4a28
Ismael de Esteban <ismael@feverup.com>
2017-03-01 16:35:16 +0100

    Add User Sagas module

---
+++ App/Sagas/UsersSagas.js
@@ -0,0 +1,17 @@
+import { call, put } from 'redux-saga/effects'
+import { path } from 'ramda'
+import TemperatureActions from '../Redux/UsersRedux'
+
+export function * getUser(api, action) {
+  const { uuid } = action
+  // make the call to the api
+  const response = yield call(api.getUserDetail, uuid)
+
+  // success?
+  if (response.ok) {
+    const user = path(['data'], response)
+    yield put(UsersActions.usersSuccess(user))
+  } else {
+    yield put(UsersActions.usersFailure())
+  }
+}
