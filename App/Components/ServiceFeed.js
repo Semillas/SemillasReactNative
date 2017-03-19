5cbcbf2a9286632706b3fb0fa6794ed3295c1756
Ismael de Esteban <ismael@feverup.com>
2017-03-19 21:40:11 +0100

    Navigate to service detail

--- App/Components/ServiceFeed.js
+++ App/Components/ServiceFeed.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
   Image
 } from 'react-native'
 import {
@@ -13,6 +14,7 @@ import {
   CardAction
 } from 'react-native-card-view'
 import Button from 'react-native-button'
+import { Actions as NavigationActions } from 'react-native-router-flux'
 
 export default class ServiceFeed extends React.Component {
 
@@ -24,34 +26,36 @@ export default class ServiceFeed extends React.Component {
     const { data } = this.props
     const card = {card: {width: 320}}
     return (
-      <View style={styles.container}>
-        <Card styles={card}>
-          <CardImage>
-            <Image
-              style={{width: 300, height: 200}}
-              source={{ uri: data.photos[0]['photo'] }}
-            />
-          </CardImage>
-          <CardTitle>
-            <Text style={styles.title}>{data.title}</Text>
-          </CardTitle>
-          <CardContent>
-            <Text>{data.description}</Text>
-          </CardContent>
-          <CardAction >
-            <Button
-              style={styles.button}
-              onPress={() => {}}>
-              Button 1
-            </Button>
-            <Button
-              style={styles.button}
-              onPress={() => {}}>
-              Button 2
-            </Button>
-          </CardAction>
-        </Card>
-      </View >
+      <TouchableHighlight onPress={NavigationActions.service} uuid={data.uuid}>
+        <View style={styles.container}>
+          <Card styles={card}>
+            <CardImage>
+              <Image
+                style={{width: 300, height: 200}}
+                source={{ uri: data.photos[0]['photo'] }}
+              />
+            </CardImage>
+            <CardTitle>
+              <Text style={styles.title}>{data.title}</Text>
+            </CardTitle>
+            <CardContent>
+              <Text>{data.description}</Text>
+            </CardContent>
+            <CardAction >
+              <Button
+                style={styles.button}
+                onPress={() => {}}>
+                Button 1
+              </Button>
+              <Button
+                style={styles.button}
+                onPress={() => {}}>
+                Button 2
+              </Button>
+            </CardAction>
+          </Card>
+        </View >
+      </TouchableHighlight>
     )
   }
 }
--- App/Containers/UsageExamplesScreen.js
+++ App/Containers/UsageExamplesScreen.js
@@ -118,7 +118,7 @@ class UsageExamplesScreen extends React.Component {
         <RoundedButton text='Listview Grid' onPress={NavigationActions.listviewGridExample} />
         <RoundedButton text='Listview Sections' onPress={NavigationActions.listviewSectionsExample} />
         <RoundedButton text='Listview Searching' onPress={NavigationActions.listviewSearchingExample} />
-        <RoundedButton text='User View' onPress={NavigationActions.user} />
+        <RoundedButton text='User View' onPress={NavigationActions.user} uuid='6753fa19-1c54-4f4e-b4ec-5caa3e0dc184' />
         <RoundedButton text='Service View' onPress={NavigationActions.service} />
         <RoundedButton text='Mapview' onPress={NavigationActions.mapviewExample} />
       </View>
