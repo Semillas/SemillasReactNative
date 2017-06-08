4886cb0a56d4aea2711e0b3021ba7b2bc0008482
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-06-08 12:17:19 +0200

    Image picker showing up

--- App/Containers/EditServiceForm.js
+++ App/Containers/EditServiceForm.js
@@ -18,6 +18,7 @@ import {Images, Metrics} from '../Themes'
 import ServiceActions from '../Redux/ServiceRedux'
 import { Actions as NavigationActions } from 'react-native-router-flux'
 import I18n from 'react-native-i18n'
+import ImagePicker from 'react-native-image-picker';
 
 type ServicePostProps = {
   dispatch: () => any,
@@ -73,6 +74,10 @@ class EditServiceForm extends React.Component {
     this.state.category = service.category.id
     this.state.uuid = service.uuid
   }
+
+
+
+
 
   componentWillReceiveProps (newProps) {
     if (newProps.service) {
@@ -179,11 +184,58 @@ class EditServiceForm extends React.Component {
     this.setState({ seedsPrice: text })
   }
 
+  selectPhoto() {
+    // More info on all the options is below in the README...just some common use cases shown here
+    var options = {
+      title: 'Select Avatar',
+      customButtons: [
+        {name: 'fb', title: 'Choose Photo from Facebook'},
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info below in README)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
+
+
+
   render () {
     const { title, description, category, seedsPrice } = this.state
     const { fetching } = this.state
     const editable = !fetching
     const textInputStyle = editable ? Styles.textInput : Styles.textInputReadonly
+
+
+
     return (
       <KeyboardAvoidingView behavior='padding'>
         <ScrollView contentContainerStyle={{justifyContent: 'center'}} style={[Styles.container, {height: this.state.visibleHeight}]} keyboardShouldPersistTaps='always'>
@@ -248,6 +300,15 @@ class EditServiceForm extends React.Component {
                 </View>
               </TouchableOpacity>
             </View>
+
+            <View style={[Styles.loginRow]}>
+              <TouchableOpacity style={Styles.loginButtonWrapper} onPress={this.selectPhoto}>
+                <View style={Styles.loginButton}>
+                  <Text style={Styles.loginText}>{I18n.t('Add Photo')}</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
