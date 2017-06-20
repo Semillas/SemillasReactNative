9cd955a6d6ec963728d67abe50d244c2f4848f58
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-06-20 23:34:35 +0200

    Add translations for Side Drawer

--- App/Containers/DrawerContent.js
+++ App/Containers/DrawerContent.js
@@ -6,6 +6,7 @@ import styles from './Styles/DrawerContentStyle'
 import { Images } from '../Themes'
 import DrawerButton from '../Components/DrawerButton'
 import { Actions as NavigationActions } from 'react-native-router-flux'
+import I18n from 'react-native-i18n'
 
 class DrawerContent extends Component {
 
@@ -62,19 +63,19 @@ class DrawerContent extends Component {
             onPress={this.handlePressProfile}
             icon='user'
           />
-          <DrawerButton text='Añadir Servicio' icon='plus-circle' onPress={this.handlePressNewService} />
-          <DrawerButton text='Servicios' icon='envira' onPress={this.handlePressFeed} />
+          <DrawerButton text={I18n.t('Add Service')} icon='plus-circle' onPress={this.handlePressNewService} />
+          <DrawerButton text={I18n.t('Services')} icon='envira' onPress={this.handlePressFeed} />
           <DrawerButton text={this.walletButtonText()} icon='money' onPress={this.handlePressCurrency} />
-          <DrawerButton text='Logout' icon='sign-out' onPress={this.props.logout} />
+          <DrawerButton text={I18n.t('Logout')} icon='sign-out' onPress={this.props.logout} />
         </ScrollView>
       )
     } else {
       return (
         <ScrollView style={styles.container}>
           <Image source={Images.logo} style={styles.logo} />
-          <DrawerButton text='Servicios' icon='envira' onPress={this.handlePressFeed} />
-          <DrawerButton text='Login' icon='sign-in' onPress={this.handlePressLogin} />
-          <DrawerButton text='Registro' icon='hand-o-right' onPress={this.handlePressLogin} />
+          <DrawerButton text={I18n.t('Services')} icon='envira' onPress={this.handlePressFeed} />
+          <DrawerButton text={I18n.t('Login')} icon='sign-in' onPress={this.handlePressLogin} />
+          <DrawerButton text={I18n.t('Sign Up')} icon='hand-o-right' onPress={this.handlePressLogin} />
           {/*
             <DrawerButton text='Component Examples' onPress={this.handlePressComponents} />
             <DrawerButton text='Usage Examples' onPress={this.handlePressUsage} />
