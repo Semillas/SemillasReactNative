b4951209ed7718c06bd4a5dce41bbd07f45adb2d
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-08-09 19:34:41 +0200

    Configuring sentry, now working

--- index.android.js
+++ index.android.js
@@ -3,5 +3,8 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config('https://2d3dcdcb83e04977ba15535574bb87d5:903189fa421f415f8b7f5f3ee0780473@sentry.io/197460').install();
 
 AppRegistry.registerComponent('SemillasReactNative', () => App)
