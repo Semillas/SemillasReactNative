2fe98e7b92485323869008b2b9157cbb894f44ac
Ismael de Esteban <ismael.deesteban@gmail.com>
2017-09-27 12:24:34 +0200

    install sentry

--- android/app/src/main/java/com/semillasreactnative/MainApplication.java
+++ android/app/src/main/java/com/semillasreactnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.semillasreactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNSentryPackage(MainApplication.this),
             new RNI18nPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
